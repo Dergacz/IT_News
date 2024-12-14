c27cdc77c01ac1467a2131bbee6e9d68d3f26cd7
Stanislau <sats96dergach@gmail.com>
2024-12-14 11:25:37 +0100

    add collapsed sidebar

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,17 +1,34 @@
 import { AppLink, classNames } from "shared";
 import styles from "./Sidebar.module.scss";
+import { useState } from "react";
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed(prev => !prev);
+  }
+
   return (
-    <div className={classNames(styles.Sidebar, {}, [className])}>
+    <div className={
+      classNames(
+        styles.Sidebar,
+        {
+          [styles.collapsed]: collapsed
+        },
+        [className]
+      )}>
       <div className={styles.linksWrapper}>
         <AppLink to="/" title="Main" />
         <AppLink to="/about" title="About" />
       </div>
+      <button onClick={onToggle}>
+        {collapsed ? 'Open' : 'Close'}
+      </button>
     </div>
   );
 };
