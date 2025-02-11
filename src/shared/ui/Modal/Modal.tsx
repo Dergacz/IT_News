80fd92710c7eeb9e48fd77d47b1d5316b7044a93
Stanislau <sats96dergach@gmail.com>
2025-02-11 20:30:12 +0100

    fix modal themes

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 import Portal from 'shared/ui/Portal/Portal';
 import { classNames } from 'shared';
 import styles from './Modal.module.scss';
+import { useTheme } from 'app/providers/ThemeProvider/useTheme';
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
+  className?: string;
   children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, className, children }) => {
+  const { theme } = useTheme();
   if (!isOpen) return null;
 
   return (
     <Portal>
-      <div className={classNames(styles.Modal)} data-testid="modal">
+      <div className={classNames(styles.Modal, {}, [className, theme])} data-testid="modal">
         <div className={styles.overlay} onClick={onClose} data-testid="modal-overlay" />
         <div className={styles.content}>
           <button className={styles.closeButton} onClick={onClose} data-testid="close-button" aria-label="Close">
