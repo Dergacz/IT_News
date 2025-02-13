d8575b7aad49fd2a73835f4b8d61a58b5aa33928
Stanislau <sats96dergach@gmail.com>
2025-02-13 15:53:28 +0100

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './shared/config/i18n/i18n';
+import { Provider } from 'react-redux';
+import store from 'app/store/store';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
+  <Provider store={store}>
     <BrowserRouter>
       <ErrorBoundary>
         <ThemeProvider>
@@ -16,4 +19,5 @@ root.render(
         </ThemeProvider>
       </ErrorBoundary>
     </BrowserRouter>
+  </Provider>
 );
